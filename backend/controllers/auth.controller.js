2d86a91caf3db714a82cdc8a3ee472fb2a8c3d04
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-08-17 21:12:13 +0530

    feat: enhance invite code verification and add token generation logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,12 +5,39 @@ import Invite from '../models/invite.model.js';
 import asynchandler from '../utility/asyncHandler.js';
 import { uploadOnCloudinary, deleteFromCloudinary } from '../utility/cloudinary.js';
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      'generateAccessAndRefreshTokens: Something went wrong while generating tokens'
+    );
+  }
+};
+
 const verifyInviteCode = asynchandler(async (req, res, next) => {
   const { inviteCode } = req.body;
-  const invite = await Invite.findOne({ code: inviteCode, expiresAt: { $gt: new Date() } });
   if (!inviteCode) {
+    return res.status(400).json({ message: 'Invite code is required' });
+  }
+
+  const invite = await Invite.findOne({ code: inviteCode, expiresAt: { $gt: new Date() } });
+  if (!invite) {
     return res.status(400).json({ message: 'Invite code is invalid or expired' });
   }
+
+  // Verified invite code, return allowed roles
+  // Once frontend get success and allowed roles, it can redirect user to registration page
+  // user fill deatails and submit registration form -> hit registerUser api
 });
 
 const registerUser = asynchandler(async (req, res) => {});
