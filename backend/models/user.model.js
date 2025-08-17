6f33024f76703987cd9c38a93834ce6e9d08faab
shaileshkvr <126746445+shaileshkvr@users.noreply.github.com>
2025-08-17 20:58:18 +0530

    fix: update password minlength requirement and add refreshToken field with default selection

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -52,7 +52,12 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-      minlength: 6,
+      select: false, // Don't return password by default
+      minlength: [8, 'Password must be at least 8 characters'],
+    },
+    refreshToken: {
+      type: String,
+      select: false, // Don't return refreshToken by default
     },
   },
   { timestamps: true }
@@ -90,6 +95,13 @@ userSchema.methods.generateAccessToken = function () {
     process.env.ACCESS_TOKEN_SECRET,
     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
   );
+};
+
+// Generate JWT Refresh Token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
 };
 
 export const User = mongoose.model('User', userSchema);
