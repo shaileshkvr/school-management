8eb7984faf153a94a2bf6ee6fc2e1e31d61291af
Shailesh Kr Verma <126746445+shaileshkvr@users.noreply.github.com>
2026-06-29 22:38:01 +0530

    Remove student attendance, fees, grades, and notice board components along with their associated API utility.

--- client/src/utils/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:9091/api",
-  withCredentials: true, // Crucial for HttpOnly cookies authentication
-});
-
-export default api;
